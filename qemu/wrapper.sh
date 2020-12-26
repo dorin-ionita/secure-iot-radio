a9c2b683d9d9828bdc4e083b27a905a4fefcd50f
Dorin Ionita <dorin.marian.ionita@gmail.com>
2020-12-27 01:35:27 +0200

    improved wrapper

--- qemu/wrapper.sh
+++ qemu/wrapper.sh
@@ -1,1 +1,6 @@
-qemu-system-aarch64 -machine virt -cpu cortex-a53 -machine type=virt -smp 1 -m 256 -kernel image.elf --append console=/dev/ttyAMA0 -serial stdio > redirect.txt
+rm redirect.txt
+touch redirect.txt
+stty 9600 -F /dev/ttyUSB0
+chmod 777 /dev/ttyUSB0
+tail -f redirect.txt > /dev/ttyUSB0 &
+qemu-system-aarch64 -machine virt -cpu cortex-a53 -machine type=virt -smp 1 -m 256 -kernel image.elf --append console=/dev/ttyAMA0 -serial stdio | tee redirect.txt
